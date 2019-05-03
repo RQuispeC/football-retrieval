bbd9a90ca47192632edd13b37a972dc61dfe65d7
jeanfranc0 <jeanfranco1417fe>
2019-05-03 17:09:03 -0300

    Update1

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from football_lib.interface.draw import generate_figures
 from football_lib.utils.iotools import read_2d
 
 # fpath = "data/data/dados_futebol/CapBotT1Suav.2d"
-fpath = "/home/vinicius/Documentos/Dados Futebol/CapBotT1Suav.2d"
+fpath = "data/data/dados_futebol/CapBotT1Suav.2d"
 # save_dir = "log/CapBotT1Suav"
-save_dir = "/home/vinicius/Documentos/log/"
+save_dir = "/home/jeanfranco/Disciplinas/Topicos en recuperacion de informacion/Proyect/log/"
 
 # load data of match
 ids, objs = read_2d(fpath)
