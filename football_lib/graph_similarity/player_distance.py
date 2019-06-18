395fd582ae53219a93d4067663b6b6954aa4b052
Vinicius Teixeira <teixeira.ufc@gmail.com>
2019-06-18 13:11:13 -0300

    range permission function

--- football_lib/graph_similarity/player_distance.py
+++ football_lib/graph_similarity/player_distance.py
@@ -14,7 +14,65 @@ distance_dic = {
 	'manhattan': distance.cityblock
 }
 
-def player_proximity(match, player, distance_function):
+def rle(inarray):
+        ia = np.asarray(inarray)
+        n = len(ia)
+        if n == 0: 
+            return (None, None, None)
+        else:
+            y = np.array(ia[1:] != ia[:-1])
+            i = np.append(np.where(y), n - 1)
+            z = np.diff(np.append(-1, i))
+            p = np.cumsum(np.append(0, z))[:-1]
+            return(z, p, ia[i])
+
+def path_processing(path, k_allowed):
+	path1 = path[:,0]
+	path2 = path[:,1]
+
+	path1_info = rle(path1)
+	
+	acc = 0
+
+	n = path1_info[0].shape[0]
+
+	for i in range(n):
+		length = path1_info[0][i]
+		if length > k_allowed:
+			init = path1_info[1][i]
+			left = init - acc
+			right = init - acc + length
+			acc = acc + length
+
+			path1 = np.delete(path1, np.arange(left,right))
+			path2 = np.delete(path2, np.arange(left,right))
+
+	path2_info = rle(path2)
+
+	acc = 0
+
+	n = path2_info[0].shape[0]
+
+	for i in range(n):
+		length = path2_info[0][i]
+		if length > k_allowed:
+			init = path2_info[1][i]
+			left = init - acc
+			right = init - acc + length
+			acc = acc + length
+
+			path1 = np.delete(path1, np.arange(left,right))
+			path2 = np.delete(path2, np.arange(left,right))
+
+	print('path1: ', path1)
+	print('path2: ', path2)
+
+	output = np.stack((path1,path2), axis=-1)
+
+	return output
+
+
+def player_proximity(match, player, k_allowed, distance_function = 'euclidean'):
 	player_signature = get_signatures(match,player)
 	function = distance_dic[distance_function]
 	
@@ -32,5 +90,6 @@ def player_proximity(match, player, distance_function):
 				path_res = path
 
 	path_res = np.array(path_res)
+	path_res = path_processing(path_res, k_allowed)
 
 	return global_distance, path_res, player_res
--- main.py
+++ main.py
@@ -9,22 +9,28 @@ from football_lib.match import Match
 from football_lib.graph_similarity.player_distance import player_proximity
 from football_lib.graph_similarity.position_distance import normal_position_proximity, gaussian_position_proximity, fastdtw_position_proximity
 
-fpath1 = "data/dados_futebol/CapBotT1Suav.2d"
-fpath2 = "data/dados_futebol/CapBotT2Suav.2d"
-save_dir = "log/"
+fpath = "/home/vinicius/Documentos/Dados Futebol/teste.2d"
+fpath1 = "/home/vinicius/Documentos/Dados Futebol/CapBotT1Suav.2d"
+fpath2 = "/home/vinicius/Documentos/Dados Futebol/CapBotT2Suav.2d"
+save_dir = "/home/vinicius/Documentos/log/"
 
 # match = Match(fpath, edge_strategy_name='knn', graph_representation_name = 'space', k = 1)
 # match = Match(fpath, edge_strategy_name='knn', graph_representation_name = 'degree', k = 1)
-#match = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'degree', thr = 40, sampling = 50, overwrite = False)
-match1 = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 50, overwrite = True)
-match2 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 50, overwrite = True)
+match = Match(fpath, edge_strategy_name='threshold', graph_representation_name = 'degree', thr = 40, sampling = 20, overwrite = False)
+# match1 = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 10, overwrite = False)
+# match2 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 10, overwrite = False)
 # match = Match(fpath, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40)
 # match = Match(fpath, edge_strategy_name='threshold', graph_representation_name = 'gEfficiency', thr = 40)
 
-#distance, path = fastdtw_position_proximity(match2, match1)
-#path = np.array(path)
-#print(path.shape)
-#print(path[:10])
+k_allowed = 5
+
+distance, path, player = player_proximity(match, 6, k_allowed, 'euclidean')
+print(path)
+
+# distance, path = fastdtw_position_proximity(match2, match1)
+# path = np.array(path)
+# print(path.shape)
+# print(path[:10])
 
 
 #import matplotlib.pyplot as plt
@@ -32,13 +38,13 @@ match2 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name
 #plt.plot(path[:30, 0], path[:30, 1])
 #plt.savefig("tmp.png")
 
-top, dis =  normal_position_proximity(match1, match2, 100, 5)
-print(match1.size())
-print(match2.size())
+# top, dis =  normal_position_proximity(match1, match2, 100, 5)
+# print(match1.size())
+# print(match2.size())
 
-for i, d in zip(top, dis):
-  print(i, "-->", d)
-  plot_comparison(match1[50], match2[i], save_dir)
+# for i, d in zip(top, dis):
+#   print(i, "-->", d)
+#   plot_comparison(match1[50], match2[i], save_dir)
 
 # plot_position(match[288], save_dir)
 # print("Presentation position 288:", match[288].signature)
