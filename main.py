9a3ead59536ed9c040d0437ed29d50dbae2c6b94
Rodolfo Quispe <quispe@sliv.ic.unicamp.br>
2019-06-18 16:31:01 +0000

    Added example for team comparison

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from football_lib.interface.draw import plot_position, plot_comparison
 from football_lib.match import Match
 
 from football_lib.graph_similarity.player_distance import player_proximity
+from football_lib.graph_similarity.team_distance import fastdtw_team_proximity
 from football_lib.graph_similarity.position_distance import normal_position_proximity, gaussian_position_proximity, fastdtw_position_proximity
 
 fpath1 = "data/dados_futebol/CapBotT1Suav.2d"
@@ -16,8 +17,8 @@ save_dir = "log/"
 # match = Match(fpath, edge_strategy_name='knn', graph_representation_name = 'space', k = 1)
 # match = Match(fpath, edge_strategy_name='knn', graph_representation_name = 'degree', k = 1)
 #match = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'degree', thr = 40, sampling = 50, overwrite = False)
-match1 = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 50, overwrite = True)
-match2 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 50, overwrite = True)
+match1 = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 50, overwrite = True, mode = 'team')
+match2 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 50, overwrite = True, mode = 'team')
 # match = Match(fpath, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40)
 # match = Match(fpath, edge_strategy_name='threshold', graph_representation_name = 'gEfficiency', thr = 40)
 
@@ -32,20 +33,16 @@ match2 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name
 #plt.plot(path[:30, 0], path[:30, 1])
 #plt.savefig("tmp.png")
 
-top, dis =  normal_position_proximity(match1, match2, 100, 5)
-print(match1.size())
-print(match2.size())
+#top, dis =  normal_position_proximity(match1, match2, 100, 5)
+#print(match1.size())
+#print(match2.size())
+#for i, d in zip(top, dis):
+#  print(i, "-->", d)
+#  plot_comparison(match1[50], match2[i], save_dir)
 
-for i, d in zip(top, dis):
-  print(i, "-->", d)
-  plot_comparison(match1[50], match2[i], save_dir)
+s = match1.get_signature()
+s_a = s[:, 1, :]
 
-# plot_position(match[288], save_dir)
-# print("Presentation position 288:", match[288].signature)
-
-# match.update_edge_strategy(edge_strategy_name='threshold', thr = 40)
-# plot_position(match[289], save_dir)
-
-# match.update_graph_representation(graph_representation_name = 'space', thr = 40)
-# plot_position(match[290], save_dir)
-# print("Presentation position 288:", match[289].signature)
+global_distance, path_res, best_team = fastdtw_team_proximity(s_a, [match2])
+print(global_distance)
+print(best_team)
