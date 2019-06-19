c09896a5797c4b13b15966ab546980ee124cb5b6
Rodolfo Quispe <quispe@sliv.ic.unicamp.br>
2019-06-19 08:55:12 +0000

    Added examples for execution interminal

--- main.py
+++ main.py
@@ -10,50 +10,42 @@ from football_lib.graph_similarity.player_distance import player_proximity
 from football_lib.graph_similarity.team_distance import fastdtw_team_proximity
 from football_lib.graph_similarity.position_distance import normal_position_proximity, gaussian_position_proximity, fastdtw_position_proximity
 
-fpath = "/home/vinicius/Documentos/Dados Futebol/teste.2d"
+fpath0 = "data/dados_futebol/CapBotT1Suav.2d"
 fpath1 = "data/dados_futebol/CapBotT1Suav.2d"
 fpath2 = "data/dados_futebol/CapBotT2Suav.2d"
+fpath3 = "data/dados_futebol/CapSpoT1Suav.2d"
+fpath4 = "data/dados_futebol/CapSpoT2Suav.2d"
 save_dir = "log/"
 
-# match = Match(fpath, edge_strategy_name='knn', graph_representation_name = 'space', k = 1)
-# match = Match(fpath, edge_strategy_name='knn', graph_representation_name = 'degree', k = 1)
-#match = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'degree', thr = 40, sampling = 30, overwrite = False)
-match1 = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 10, overwrite = False, mode = 'team')
-match2 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 10, overwrite = False, mode = 'team')
-# match = Match(fpath, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40)
-# match = Match(fpath, edge_strategy_name='threshold', graph_representation_name = 'gEfficiency', thr = 40)
+match0 = Match(fpath1, edge_strategy_name='knn', graph_representation_name = 'degree', k = 40, sampling = 10)
+match1 = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 10, overwrite = True, mode = 'position')
+match2 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 10, overwrite = True, mode = 'position')
+match3 = Match(fpath1, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 10, overwrite = True, mode = 'team')
+match4 = Match(fpath2, edge_strategy_name='threshold', graph_representation_name = 'embedding', thr = 40, sampling = 10, overwrite = True, mode = 'team')
 
-k_allowed = 10
+#plot a position
+plot_position(match0[100], save_dir)
 
-#distance, path, player = player_proximity(match, 6, k_allowed, 'euclidean')
-
-#generate_video(match, match, path, save_dir, match.team_size_limit, match.team_size_limit, 6, 0, player, 0)
-
-# distance, path = fastdtw_position_proximity(match2, match1)
-# path = np.array(path)
-# print(path.shape)
-# print(path[:10])
-
-
-#import matplotlib.pyplot as plt
-#plt.title('similarity over time')
-#plt.plot(path[:30, 0], path[:30, 1])
-#plt.savefig("tmp.png")
+# Player similarity
+k_allowed = 5
+player_query = 6
+distance, path, best_player = player_proximity(match0, player_query, k_allowed, 'euclidean')
+generate_video(match0, match0, path, save_dir, match0.team_size_limit, match0.team_size_limit, player_query, 0, best_player, 0)
 
 # Position proximity
-# top, dis =  normal_position_proximity(match1, match2, 100, 5)
-# print(match1.size())
-# print(match2.size())
-
-# for i, d in zip(top, dis):
-#   print(i, "-->", d)
-#   plot_comparison(match1[50], match2[i], save_dir)
+query = 100
+top_size = 5
+top, dis =  normal_position_proximity(match1, match2, query, top_size)
+for ind, (i, d) in enumerate(zip(top, dis)):
+  print(i, "-->", d)
+  plot_comparison(ind, match1[query], match2[i], save_dir, match1.team_size_limit, match2.team_size_limit)
 
 # Team proximity
-s = match1.get_signature()
-s_a = s[:, 1, :]
-
-global_distance, path_res, best_team = fastdtw_team_proximity(s_a, [match2], k_allowed)
+s = match3.get_signature()
+team = 0
+s_a = s[:, team, :]
+k_allowed = 3
+global_distance, path, best_team = fastdtw_team_proximity(s_a, [match4], k_allowed)
 print(global_distance)
 print(best_team)
-generate_video(match1, match2, path_res, save_dir, match1.team_size_limit, match2.team_size_limit)
+generate_video(match1, match2, path, save_dir, match3.team_size_limit, match4.team_size_limit)
